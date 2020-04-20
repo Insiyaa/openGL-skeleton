82303aa47a8727ef5a86d4780012a9d970f9b691
Insiyaa <insiyahhajoori@gmail.com>
2020-04-20 15:37:11 +0530

    Get User Input

--- window.cpp
+++ window.cpp
@@ -4,16 +4,26 @@ Window::Window()
 {
 	width = 800;
 	height = 600;
+
 	bufferWidth = 0, bufferHeight = 0;
+
 	mainWindow = 0;
+
+	for (size_t i = 0; i < 1024; i++)
+        keys[i] = 0;
 }
 
 Window::Window(GLint windowWidth, GLint windowHeight)
 {
 	width = windowWidth;
 	height = windowHeight;
+
 	bufferWidth = 0, bufferHeight = 0;
+
 	mainWindow = 0;
+
+	for (size_t i = 0; i < 1024; i++)
+        keys[i] = 0;
 }
 
 int Window::initialize()
@@ -51,6 +61,10 @@ int Window::initialize()
     // set context for glew to use
     glfwMakeContextCurrent(mainWindow);
 
+    // handle key + mouse input
+    CreateCallbacks();
+    glfwSetInputMode(mainWindow, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
     // allow modern extension features
     glewExperimental = GL_TRUE;
 
@@ -69,9 +83,74 @@ int Window::initialize()
     // setup viewport size
     glViewport(0, 0, bufferWidth, bufferHeight);
 
+    // owner of the window is 'this' class
+    // helps the callback function to know the class
+    glfwSetWindowUserPointer(mainWindow, this);
+
 	return 0;
 }
 
+void Window::CreateCallbacks()
+{
+    glfwSetKeyCallback(mainWindow, HandleKeys);
+    glfwSetCursorPosCallback(mainWindow, HandleMouse);
+}
+
+GLfloat Window::GetXChange()
+{
+    GLfloat theChange = xChange;
+    xChange = 0.0f;
+    return theChange;
+}
+
+GLfloat Window::GetYChange()
+{
+    GLfloat theChange = yChange;
+    yChange = 0.0f;
+    return theChange;
+}
+
+void Window::HandleKeys(GLFWwindow *window, int key, int code, int action, int mode)
+{
+    // to access keys[], getting the current window object
+    Window* theWindow = static_cast<Window*>(glfwGetWindowUserPointer(window));
+
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, GL_TRUE);
+    }
+
+    if (key >= 0 && key < 1024)
+    {
+        if (action == GLFW_PRESS)
+        {
+            theWindow -> keys[key] = true;
+        }
+        else if (action == GLFW_RELEASE)
+        {
+            theWindow->keys[key] = false;
+        }
+    }
+}
+
+void Window::HandleMouse(GLFWwindow *window, double xPos, double yPos)
+{
+    Window* theWindow = static_cast<Window*>(glfwGetWindowUserPointer(window));
+
+    if (theWindow->mouseFirstMoved)
+    {
+        theWindow->lastX = xPos;
+        theWindow->lastY = yPos;
+        theWindow->mouseFirstMoved = false;
+    }
+    theWindow->xChange = xPos - theWindow->lastX;
+    // to prevent inverted up and down movements, depends on coordinate sys
+    theWindow->yChange = theWindow->lastY - yPos;
+
+    theWindow->lastX = xPos;
+    theWindow->lastY = yPos;
+
+}
 
 Window::~Window()
 {
--- window.h
+++ window.h
@@ -18,13 +18,37 @@ public:
 	GLint GetBufferWidth() { return bufferWidth; }
 	GLint GetBufferHeight() { return bufferHeight; }
 	bool GetShouldClose() { return glfwWindowShouldClose(mainWindow); }
+
+	bool* GetKeys() { return keys; }
+	GLfloat GetXChange();
+	GLfloat GetYChange();
+
 	void SwapBuffers() { glfwSwapBuffers(mainWindow); }
 
 	virtual ~Window();
 private:
 	GLFWwindow* mainWindow;
+
 	GLint width, height;
 	GLint bufferWidth, bufferHeight;
+
+	bool keys[1024];
+
+	GLfloat lastX,
+            lastY,
+            xChange,
+            yChange;
+
+    bool mouseFirstMoved;
+
+	void CreateCallbacks();
+
+	// static because we have to use as a callback. normal member functions won't work
+	// parameters according to callback req
+	// A static function is a member function of a class that can be called even when an object of the class is not initialized. It doesn't know the object
+	static void HandleKeys(GLFWwindow *window, int key, int code, int action, int mode);
+
+	static void HandleMouse(GLFWwindow *window, double xPos, double yPos);
 };
 
 #endif // WINDOW_H
